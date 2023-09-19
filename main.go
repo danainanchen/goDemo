01bb9cd32cb58689a43c8e24b76e9e0449100aa0
danainan <danainan555@gmail.com>
2023-09-19 16:26:26 +0700

    modified:   go.sum 	modified:   main.go

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"encoding/json"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/requestid"
 )
 
 type Person struct {
@@ -9,19 +13,42 @@ type Person struct {
 	Lname string
 }
 
+var person Person
+
 func getPerson(ctx *fiber.Ctx) error {
-	person_1 := Person{
-		Fname: "Danainan",
-		Lname: "Chamnanpaison",
+	return ctx.Status(fiber.StatusOK).JSON(person)
+}
+
+func postPerson(ctx *fiber.Ctx) error {
+	body := new(Person)
+	err := ctx.BodyParser(body)
+	if err != nil {
+		ctx.Status(fiber.ErrBadRequest.Code).SendString(err.Error())
+		return err
 	}
-	return ctx.Status(fiber.StatusOK).JSON(person_1)
+
+	person = Person{
+		Fname: body.Fname,
+		Lname: body.Lname,
+	}
+	return ctx.Status(fiber.StatusOK).JSON(person)
 }
 
 func main() {
-	app := fiber.New()
-	app.Get("/", func(ctx *fiber.Ctx) error {
-		return ctx.SendString("hello")
+	app := fiber.New(fiber.Config{
+		JSONEncoder: json.Marshal,
+		JSONDecoder: json.Unmarshal,
 	})
-	app.Get("/person", getPerson)
-	app.Listen(":80")
+	app.Get("/", func(ctx *fiber.Ctx) error {
+		return ctx.SendString("hello World")
+	})
+
+	app.Use(logger.New())
+	app.Use(requestid.New())
+
+	personApp := app.Group("/person")
+	personApp.Get("", getPerson)
+	personApp.Post("", postPerson)
+
+	app.Listen(":8080")
 }
